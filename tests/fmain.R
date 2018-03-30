b9f7d9fd1daf5bd8f69e478d38f76475a81366a1
hb <hb@aroma-project.org>
2018-03-30 13:56:57 -0700

    TESTS: write.table() with 13 digits seems a bit much; lowering to 12 digits [#1, #2]
    ```diff
    $ diff tests/.checks/fmain.R.out Shendure/work/Henrik/outcontact_hap1_full_all_22_100000.txt
    1c1
    < Prefix of output files: 'tests/.checks/hap1_full_all_22_100000'
    ---
    > [1] TRUE
    394,395d393
    <
    <
    397c395
    < - file 1: 'hap1_full_all_22_100000.txt' (23581 bytes; 52e5b10c66211940ac3c50cdd9e57c44)
    ---
    > - file 1: 'hap1_full_all_22_100000.txt' (23580 bytes; 8d7768f111eff4e81d39909a2b1f8aa3)
    401,402d398
    <
    <
    427,430d422
    <
    < Processing time:
    <    user  system elapsed
    < 264.128   0.168 264.383
    
    $ diff -w -u tests/.checks/hap1_full_all_22_100000.txt Shendure/work/Henrik/hap1_full_all_22_100000.txt
    --- tests/.checks/hap1_full_all_22_100000.txt	2018-03-30 13:52:46.476120758 -0700
    +++ Shendure/work/Henrik/hap1_full_all_22_100000.txt	2018-03-30 13:34:02.425136889 -0700
    @@ -26,8 +26,8 @@
     18700000	18800000	0.0202070346974	1.23167055517	0.1522614198528
     18800000	18900000	0.0899570839747	1.0883979893771	0.1736695674973
     18900000	1.9e+07	-0.0174709155585	0.8335720596815	0.3617872147332
    -1.9e+07	19100000	0.0126902241769	0.8919774368768	0.3562127728153
    -19100000	19200000	-0.0103047609353	0.8751153108721	0.3168368469317
    +1.9e+07	19100000	0.0126902241769	0.8919774368767	0.3562127728153
    +19100000	19200000	-0.0103047609353	0.875115310872	0.3168368469317
     19200000	19300000	0.0162483912116	0.8659055417259	0.2854422820201
     19300000	19400000	0.0300898691889	0.8900892407292	0.3022622668437
     19400000	19500000	0.0683860519144	0.8855503356063	0.2895407693471
    @@ -155,7 +155,7 @@
     31600000	31700000	-0.3160278154083	-0.0958838987677	-0.0163705036568
     31700000	31800000	-0.3398958891951	-0.1051723600417	-0.0638253211242
     31800000	31900000	-0.3704334220314	-0.0826529142275	-0.0725215548749
    -31900000	3.2e+07	-0.4042954913038	-0.1106541598177	-0.0979725120991
    +31900000	3.2e+07	-0.4042954913038	-0.1106541598177	-0.0979725120992
     3.2e+07	32100000	-0.4368102579397	-0.1141003981312	-0.1307936818438
     32100000	32200000	-0.4576964293582	-0.1507286313248	-0.1437309521018
     32200000	32300000	-0.4893847407921	-0.1646746213303	-0.1305293689477
    @@ -292,7 +292,7 @@
     45300000	45400000	0.0145672466276	-0.6075546002136	0.4262554799769
     45400000	45500000	0.0185440090027	-0.5661946612904	0.4273023069252
     45500000	45600000	0.042059056936	-0.5759995240491	0.4631951601992
    -45600000	45700000	0.0866286928632	-0.5944283915585	0.4655572804859
    +45600000	45700000	0.0866286928632	-0.5944283915585	0.4655572804858
     45700000	45800000	0.1018871281601	-0.546658227533	0.4854721032671
     45800000	45900000	0.1282331227524	-0.5834367889962	0.5026387638767
     45900000	4.6e+07	0.1233039504185	-0.5756782894165	0.4495385045958
    
    $ diff -w -u tests/.checks/hap1_full_all_22_100000beta.txt Shendure/work/Henrik/hap1_full_all_22_100000beta.txt
    ```

--- tests/fmain.R
+++ tests/fmain.R
@@ -1,8 +1,12 @@
 source("hsa/cstruct1.R")
 
+outpath <- file.path("tests", ".checks")
+dir.create(outpath, recursive = TRUE)
+outprefix <- file.path(outpath, "hap1_full_all_22_100000")
+cat(sprintf("Prefix of output files: %s\n", sQuote(outprefix)))
+
+# Input data
 contacts <- file.path("inst", "exdata", "contact_hap1_full_all_22_100000.txt")
-outprefix <- tempfile(pattern = "cstruct1_fmain_")
-message("Prefix of output files: ", sQuote(outprefix))
 
 ## Parameters
 mak <- 1L
@@ -16,12 +20,39 @@ lsmap0 <- list(data)
 
 set.seed(12345)
 
+options(digits = 7L, scipen = 0L) ## Reproducible print() output
+
+## AD HOC: Trick cstruct1.R code to write files with 12 digits
+## (still plenty) instead of 15 digits for easier 'diff' comparisons
+write.table <- function(x, ...) {
+  for (kk in seq_along(x)) {
+    if (is.double(x[[kk]])) x[[kk]] <- round(x[[kk]], digits = 12L)
+  }
+  utils::write.table(x, ...)
+}
+
 ## Benchmarking history:
-## commit 0880732: ~ 270 secs = 4.5 mins
+## commit 0880732: ~270 secs
+## commit ccc77de: ~265 secs
 dt <- system.time({
   out <- fmain(lsmap0 = lsmap0, lscov0 = lscov0, outfile = outprefix,
                Maxiter = 3L, submaxiter = 100L, lambda = 25, Leapfrog = 20,
                epslon = 0.003, mkfix = 0, rho = 0, mk = mak)
 })
+print(out)
+cat("\n\n")
+
+cat("Files produced:\n")
+file1 <- sprintf("%s.txt", outprefix)
+file2 <- sprintf("%sbeta.txt", outprefix)
+cat(sprintf("- file 1: %s (%d bytes; %s)\n",
+            sQuote(basename(file1)), file.size(file1), tools::md5sum(file1)))
+cat(sprintf("- file 2: %s (%d bytes; %s)\n",
+            sQuote(basename(file2)), file.size(file2), tools::md5sum(file2)))
+cat("\n\n")
+
+cat("\n\n")
+print(sessionInfo())
+
+cat("\nProcessing time:\n")
 print(dt)
-saveRDS(out, file = sprintf("%s.out.rds", outfile))
